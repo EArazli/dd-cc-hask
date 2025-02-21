fd2ca40498e5c312cb623cff45f5ba9867f48149
Emin Arazli <emin@medal.tv>
2025-02-21 12:35:46 +0100

    use type family and implement morphism

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,12 +12,29 @@ import Control.Arrow.Constrained
 import Control.Category.Constrained.Prelude
 import qualified Control.Category.Hask as Hask
 import Control.Monad.Constrained
+import Data.Bifunctor (bimap)
 import Prelude ()
 
-newtype Delta x = Delta x
-  deriving (Show)
+infixl 6 ⊖, ⊕
 
-newtype Inc x y = Inc (x -> (y, Delta x -> Delta y))
+class HasDelta a where
+  type D a
+  (⊕) :: a -> D a -> a
+  (⊖) :: a -> a -> D a
+
+instance (HasDelta a, HasDelta b) => HasDelta (a, b) where
+  type D (a, b) = (D a, D b)
+  (⊕) :: (a, b) -> D (a, b) -> (a, b)
+  (a, b) ⊕ (da, db) = (a ⊕ da, b ⊕ db)
+  (⊖) :: (a, b) -> (a, b) -> D (a, b)
+  (a, b) ⊖ (da, db) = (a ⊖ da, b ⊖ db)
+
+instance HasDelta () where
+  type D () = ()
+  () ⊕ () = ()
+  () ⊖ () = ()
+
+newtype Inc x y = Inc (x -> (y, D x -> D y))
 
 instance Category Inc where
   id :: Inc a a
@@ -28,13 +45,14 @@ instance Category Inc where
       (fgx, df) -> (fgx, df . dg)
 
 instance Cartesian Inc where
-  swap = Inc $ \(a, b) -> ((b, a), \(Delta (ax, bx)) -> Delta (bx, ax))
-  attachUnit = Inc $ \a -> ((a, ()), \(Delta ax) -> Delta (ax, ()))
-  detachUnit = Inc $ \(a, _) -> (a, \(Delta (ax, _)) -> Delta ax)
-  regroup = Inc $ \(a, (b, c)) -> (((a, b), c), \(Delta (ax, (bx, cx))) -> Delta ((ax, bx), cx))
-  regroup' = Inc $ \((a, b), c) -> ((a, (b, c)), \(Delta ((ax, bx), cx)) -> Delta (ax, (bx, cx)))
+  swap = Inc $ \(a, b) -> ((b, a), \(ax, bx) -> (bx, ax))
+  attachUnit = Inc $ \a -> ((a, ()), (,()))
+  detachUnit = Inc $ \(a, _) -> (a, fst)
+  regroup = Inc $ \(a, (b, c)) -> (((a, b), c), \(ax, (bx, cx)) -> ((ax, bx), cx))
+  regroup' = Inc $ \((a, b), c) -> ((a, (b, c)), \((ax, bx), cx) -> (ax, (bx, cx)))
 
 instance Morphism Inc where
-  Inc f *** Inc g = Inc $ \(b, c) -> case f b of
-    (b', db') -> case g c of
-      (c', dc') -> ((b', c'), \(Delta (x, y)) -> Delta (_, _))
+  Inc f *** Inc g = Inc $ \(a, b) ->
+    let (c, dc) = f a
+        (d, dd) = g b
+     in ((c, d), bimap dc dd)
