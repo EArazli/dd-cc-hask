5d58710924b26f239e0e1ae83b61ce5bdae2178f
Emin Arazli <emin@medal.tv>
2025-03-05 17:13:41 +0100

    start html dsl experiments

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,47 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import Control.Category.Constrained.Prelude
-import GAD
-import Template
-import Prelude ()
+import           Control.Monad.Trans.Reader (runReader)
+import Blaze.ByteString.Builder (fromByteString)
+import Control.Concurrent (threadDelay)
+import Network.HTTP.Types (status200)
+import Network.Wai
+import Network.Wai.Handler.Warp (run)
+import           Data.Text                   (Text)
+import           Text.Html.Nice              ((:$) (..), Attr (..),
+                                           Render (..), render)
+import Text.Html.Nice.Internal (FastMarkup(..))
+import           Text.Html.Nice.Writer
+import           Text.Html.Nice.Writer.Html5
+import Text.Html.Nice.Shadow
+import Data.Text.Internal.Builder
+import Data.Void
 
-calc :: Inc Double Double
-calc = alg (\x -> x * x + x)
+-- application _ respond = respond $
+--   responseStream status200 [("Content-Type", "text/html")] $
+--     \send flush -> do
+--       send $ fromByteString "<!DOCTYPE html><html><body><template shadowrootmode=\"open\"><h1>My First Heading</h1><slot name=\"content\">Loading...</slot><p>My first paragraph.</p></template></body></html>"
+--       flush
+--       threadDelay 1000000
+--       send $ fromByteString "<p slot=\"content\">Loaded!</p>"
 
--- main :: IO ()
--- main = do
---   let (D f) = calc
---   let (result, DelX next) = f 4
---   let nextResult = next 4
---   print result
---   print nextResult
+-- main = run 3000 application
 
-main :: IO ()
-main = p
+data Env = Env {
+  greeting :: Text
+}
+
+template :: (FastMarkup (Env -> Text))
+template = compile $ do
+  doctype_
+  html_ $ do
+    head_ $ title_ "Todo List"
+    body_ $ do
+      h1_ "Todo List!"
+      b_ (dynamic greeting)
+
+
+main = print "Hello"
